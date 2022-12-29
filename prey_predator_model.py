22cec4d2d4d44941f3dc8fd05a5888c25bb8699d
AldonaSwirad <aldona.ewa.s@gmail.com>
2022-12-29 21:10:34 +0100

    add sliders

---
+++ prey_predator_model.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.widgets import Slider, Button
+#from mpl_interactions import ipyplot as iplt
+
+from scipy import integrate
+
+fox = 5
+rabbit = 10
+
+t = np.linspace(0, 15, 1000)  # time
+X0 = np.array([rabbit, fox])  # initials conditions: 10 rabbits and 5 foxes
+
+# use `c_` instead of `c` because `c` is an argument to plt.scatter
+
+# Create a subplot
+fig, ax = plt.subplots()
+
+# initials conditions
+a = 1.5
+b = 0.5
+c_ = 0.5
+d = 0.5
+
+plt.subplots_adjust(bottom=0.35) 
+# lotka-volttera differential equestion
+def f(a, b, c_, d):   
+    def dX_dt(X, t=0):
+        rabbits, foxes = X
+        dRabbit_dt = a * rabbits - b * foxes * rabbits
+        dFox_dt = -c_ * foxes + d * b * rabbits * foxes
+        return [dRabbit_dt, dFox_dt]
+
+    X, legenda = integrate.odeint(dX_dt, X0, t, full_output=True)
+    return X  # expects shape (N, 2)
+
+plt.plot(f(a,b,c_,d))
+
+# ax of sliders
+ax_a = plt.axes([0.25, 0.2, 0.65, 0.03])
+ax_b = plt.axes([0.25, 0.15, 0.65, 0.03])
+ax_c = plt.axes([0.25, 0.1, 0.65, 0.03])
+ax_d = plt.axes([0.25, 0.05, 0.65, 0.03])
+
+# Sliders
+a_slider = Slider(
+    ax=ax_a,
+    label = 'a',
+    valmin=0,
+    valmax=3,
+    valinit=1
+)
+
+b_slider = Slider(
+    ax=ax_b,
+    label = 'b',
+    valmin=0,
+    valmax=3,
+    valinit=0.5
+
+)
+
+c_slider = Slider(
+    ax=ax_c,
+    label = 'c',
+    valmin=0,
+    valmax=3,
+    valinit=0.5
+
+)
+d_slider = Slider(
+    ax=ax_d,
+    label = 'd',
+    valmin=0,
+    valmax=3,
+    valinit=0.5
+
+)
+
+# a_slider.set_val(a)
+# b_slider.set_val(b)
+# c_slider.set_val(c_)
+# d_slider.set_val(d)
+
+def update(val):
+    a = a_slider.val
+    b = b_slider.val
+    c = c_slider.val
+    d = d_slider.val
+    ax.plot(f(a,b,c_,d))
+ 
+
+a_slider.on_changed(update)
+b_slider.on_changed(update)
+c_slider.on_changed(update)
+d_slider.on_changed(update)
+
+
+plt.show()
