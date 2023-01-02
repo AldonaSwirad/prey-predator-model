94e08b721b76b9bf0c03031421777fbc66cbf91c
kosiarn <62908964+kosiarn@users.noreply.github.com>
2023-01-02 11:53:23 +0100

    code cleanup
    duplicate resetSlider() function definition deleted; modified resetSlider() to clear both plots

--- prey_predator_model.py
+++ prey_predator_model.py
@@ -36,7 +36,7 @@ def f(a, b, c_, d):
     return X  # expects shape (N, 2)
 
 
-#
+
 
 model_calc_result = f(a, b, c_, d)
 ax[0].plot(model_calc_result)  # predator and prey population over time
@@ -68,7 +68,8 @@ def update(val):
     b = b_slider.val
     c_ = c_slider.val
     d = d_slider.val
-# phase-diagram
+
+    # phase-diagram
     model_calc_result = f(a, b, c_, d)
     ax[0].clear()
     ax[0].plot(model_calc_result)
@@ -76,9 +77,6 @@ def update(val):
     ax[1].plot(model_calc_result[:, 0], model_calc_result[:, 1])
 
 
-    ax.clear()
-    ax.plot(f(a,b,c_,d))
- 
 # main
 
 a_slider.on_changed(update)
@@ -95,26 +93,12 @@ def resetSlider(event):
     b_slider.reset()
     c_slider.reset()
     d_slider.reset()
-    ax.clear()
-    ax.plot(f(a,b,c_,d))
+    update(resetax)
  
 # Call resetSlider function when clicked on reset button
 button.on_clicked(resetSlider)
 
 
-# Create axes for reset button and create button
-resetax = plt.axes([0.05, 0.05, 0.1, 0.04])
-button = Button(resetax, 'Reset', color='gold', hovercolor='skyblue')
 
-def resetSlider(event):
-    a_slider.reset()
-    b_slider.reset()
-    c_slider.reset()
-    d_slider.reset()
-    ax.clear()
-    ax.plot(f(a,b,c_,d))
- 
-# Call resetSlider function when clicked on reset button
-button.on_clicked(resetSlider)
 
 plt.show()
